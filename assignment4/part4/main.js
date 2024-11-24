e4454ec7c407afc1f05bd8eb5376f80679c52fd7
Hetvi9705 <hetvipiyushkumar.patel@dcmail.ca>
2024-11-24 13:37:37 -0500

    changes to make it work

--- assignment4/part4/main.js
+++ assignment4/part4/main.js
@@ -18,21 +18,22 @@ function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
 
-class Ball extends Shape {
-  constructor(x, y, velX, velY, size, color) {
-      super(x, y, velX, velY);
-      this.size = size;
-      this.color = color;
-      this.exists = true;
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
   }
 
   draw() {
-    if (this.exists) {
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
-        ctx.fill();
-    }
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 
   update() {
     if (this.x + this.size >= width) {
@@ -57,7 +58,7 @@ class Ball extends Shape {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball) && ball.exits) {
+      if (!(this === ball)) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -69,31 +70,6 @@ class Ball extends Shape {
     }
   }
 }
-
-//class EvilCircle extends Shape {
-  //constructor(x, y) {
-    //  super(x, y, 20, 20);
-      //this.color = 'white';
-      //this.size = 10;
-
-      window.addEventListener('keydown', (e) => {
-          switch (e.key) {
-              case 'a':
-                  this.x -= this.velX;
-                  break;
-              case 'd':
-                  this.x += this.velX;
-                  break;
-              case 'w':
-                  this.y -= this.velY;
-                  break;
-              case 's':
-                  this.y += this.velY;
-                  break;
-          }
-      });
-  }
-
 
 const balls = [];
 
