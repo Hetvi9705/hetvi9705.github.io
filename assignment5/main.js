90b885325786c1f60a0d271386eed6fc6bda11b4
Hetvi9705 <hetvipiyushkumar.patel@dcmail.ca>
2024-12-02 15:06:46 -0500

    create the main.js file
    constants are added

---
+++ assignment5/main.js
@@ -0,0 +1,4 @@
+// functionality for showing/hiding the comments section
+
+const showHideBtn = document.querySelector('.show-hide');
+const commentWrapper = document.querySelector('.comment-wrapper');
